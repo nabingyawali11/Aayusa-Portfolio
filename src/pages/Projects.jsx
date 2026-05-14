4be461f2c61e103be7e639c2a6afb0af21228174
nabingyawali11 <gyawalibhagiratha@gmail.com>
2026-05-15 02:39:37 +0545

    Update section title from 'Education' to 'Projects' in Projects component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
       <div className="max-w-4xl mx-auto px-6 lg:px-8">
         <div className="text-center mb-16">
           <h2 className="text-4xl font-black tracking-tighter text-gray-900 uppercase">
-            [ Education ]
+            [ Projects ]
           </h2>
         </div>
 
